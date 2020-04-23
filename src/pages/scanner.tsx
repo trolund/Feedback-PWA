3416abe76c3f0f621ed98276abdcbedda867c108
Troels Lund <trolund@gmail.com>
2020-04-23 10:46:09 +0200

    scanner update

--- src/pages/scanner.tsx
+++ src/pages/scanner.tsx
@@ -2,6 +2,7 @@ import dynamic from 'next/dynamic'
 import { observer } from 'mobx-react-lite'
 import Router from 'next/router'
 import Page from '../components/page'
+import Section from '../components/section'
 
 const QrReader = dynamic(() => import('react-qr-reader'), {
   ssr: false
@@ -21,24 +22,40 @@ const Scanner = observer(() => {
     console.error(err)
   }
   return (
-    <Page title='' showBottomNav={false} fullscreen showBackButton>
-      <QrReader
-        className='container'
-        delay={300}
-        onError={handleError}
-        onScan={handleScan}
-      />
+    <Page
+      title='QR kode scanner'
+      showBottomNav={false}
+      fullscreen
+      showBackButton
+    >
+      <Section>
+        <div className='container'>
+          <QrReader
+            className='cam-view'
+            delay={300}
+            onError={handleError}
+            onScan={handleScan}
+          />
+          <hr />
+          <p style={{ textAlign: 'center', width: '100%' }}>
+            Scan QR kode for et møde for at give feedback
+          </p>
+        </div>
+      </Section>
+
       <style jsx>{`
         .container {
+          position: absolute;
+          left: 0;
+          right: 0;
           padding-top: 50px;
           position: absolute;
           min-width: 100%;
           min-height: 100%;
         }
-        .container > section {
-          position: absolute !important;
-          min-width: 100%;
-          min-height: 100%;
+
+        .cam-view section div {
+          box-shadow: rgba(0, 255, 0, 0.5) 0px 0px 0px 5px inset !important;
         }
       `}</style>
     </Page>
