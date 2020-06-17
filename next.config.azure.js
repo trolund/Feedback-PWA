9c1ba87a69814538d67fbf2da5d533cd8d5f1d41
Troels Lund <trolund@gmail.com>
2020-06-17 07:50:35 +0200

    update cache config azure

--- next.config.azure.js
+++ next.config.azure.js
@@ -92,26 +92,28 @@ module.exports = withImages(
         },
         {
           urlPattern: /.*\/Api\/.*$/i,
-          handler: 'NetworkOnly',
+          handler: 'NetworkFirst',
           method: 'GET',
           options: {
             cacheName: 'apis',
             expiration: {
               maxEntries: 16,
               maxAgeSeconds: 24 * 60 * 60 // 24 hours
-            }
+            },
+            networkTimeoutSeconds: 10 // fall back to cache if api does not response within 10 seconds
           }
         },
         {
           urlPattern: /.*\/Api\/.*$/i,
-          handler: 'NetworkOnly',
+          handler: 'NetworkFirst',
           method: 'POST',
           options: {
             cacheName: 'apis',
             expiration: {
               maxEntries: 16,
               maxAgeSeconds: 24 * 60 * 60 // 24 hours
-            }
+            },
+            networkTimeoutSeconds: 10 // fall back to cache if api does not response within 10 seconds
           }
         },
         {
