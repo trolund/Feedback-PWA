65b3f151acf5c9618ac5aed26f2346e15e8132bc
Troels Lund <trolund@gmail.com>
2020-06-19 15:38:08 +0200

    added meta data

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -30,6 +30,7 @@ const App = ({ Component, pageProps }: AppProps) => {
         <meta name='application-name' content='pub' />
         {/* <meta name='description' content='Company internal hub' />
         <meta name='theme-color' content='#457ef7' /> */}
+        <meta name='theme-color' content='#0594a6' />
         <meta
           name='viewport'
           content='width=device-width, initial-scale=1, user-scalable=0, viewport-fit=cover'
